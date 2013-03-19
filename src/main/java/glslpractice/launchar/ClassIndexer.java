dcbc4a157834a1435be54619bb9591cdb3798cbf
urasa <ursk4git@live.jp>
2013-03-20 00:04:04 +0900

    指定したパッケージ以下のmain()を持つクラスを列挙できるようになった

--- src/main/java/glslpractice/launchar/ClassIndexer.java
+++ src/main/java/glslpractice/launchar/ClassIndexer.java
@@ -1,7 +1,9 @@
 package glslpractice.launcher;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Enumeration;
@@ -11,15 +13,39 @@ import java.util.List;
  * 指定されたパッケージからクラスの一覧を作成する
  */
 public class ClassIndexer {
-    public static List<String> getIndex(String path) throws IOException {
+    public static List<String> getIndex(String packageName) throws IOException {
         // パッケージはリソース中のディレクトリ構造を表しているので
         // クラスファイルはそのディレクトリからリソースとして取得できる
-        String classdir = path.replace('.', '/');
+        String classdir = packageName.replace('.', '/');
         Enumeration<URL> resources = Thread.currentThread().getContextClassLoader().getResources(classdir);
+        List<File> dirs = new ArrayList<>();
         for (URL url : Collections.list(resources)) {
-            System.out.println(url.toString());
+            dirs.add(new File(URLDecoder.decode(url.getFile(), "UTF-8")));
         }
+        ArrayList<String> classes = new ArrayList<>();
+        for (File dir : dirs) {
+            classes.addAll(getClassFiles(dir, packageName));
+        }
+        return classes;
+    }
 
-        return null;
+    /**
+     * 指定されたディレクトリからクラスファイルのパスの一覧を取得する
+     */
+    private static List<String> getClassFiles(File dir, String packageName) {
+        List<String> classes = new ArrayList<>();
+        File[] files = dir.listFiles();
+        for (File file : files) {
+            String filename = file.getName();
+            if (file.isDirectory()) {
+                classes.addAll(getClassFiles(file, packageName + "." + filename));
+            }
+            else if (file.isFile()) {
+                if (filename.endsWith(".class") && !filename.contains("$")) {
+                    classes.add(packageName + "." + filename.replaceAll(".class$", ""));
+                }
+            }
+        }
+        return classes;
     }
 }
--- src/main/java/glslpractice/launchar/Launcher.java
+++ src/main/java/glslpractice/launchar/Launcher.java
@@ -1,17 +1,57 @@
 package glslpractice.launcher;
 
+import java.lang.reflect.Method;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 指定されたパスからmainを持ったクラスの一覧を取得する
  */
 public class Launcher {
     public static void main(String[] args) {
+        List<String> classNames = null;
         try {
-            ClassIndexer.getIndex("glslpractice");
+            classNames = ClassIndexer.getIndex("glslpractice");
         }
         catch (IOException e) {
             e.printStackTrace();
         }
+        if (classNames != null && classNames.isEmpty()) {
+            System.out.println("No classes found.");
+            return;
+        }
+        List<Class<?>> classes = getMainClasses(classNames);
+        for (Class c : classes) {
+            System.out.println(c.getName() + " has main()");
+        }
+    }
+    private static List<Class<?>> getMainClasses(List<String> classNames) {
+        List<Class<?>> classes = new ArrayList<>();
+        Class<?> c = null;
+        for (String className : classNames) {
+            try {
+                c = Class.forName(className);
+            }
+            catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            if (c != null && isMainClass(c)) {
+                classes.add(c);
+            }
+        }
+        return classes;
+    }
+    /**
+     * mainを持ったクラスかを調べる
+     * TODO mainがオーバーロードされている場合に誤検出について検証・対処
+     */
+    private static boolean isMainClass(Class<?> c) {
+        for (Method method : c.getMethods()) {
+            if (method.getName() == "main") {
+                return true;
+            }
+        }
+        return false;
     }
 }
