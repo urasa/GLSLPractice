94fc8af22d51dd531d1f47ea83096e1d6a636308
urasa <ursk4git@live.jp>
2013-03-19 22:41:36 +0900

    読み込み元のパッケージ名を起動時の引数ではなく'glslpractice'に指定

--- src/main/java/glslpractice/launchar/Launcher.java
+++ src/main/java/glslpractice/launchar/Launcher.java
@@ -1,6 +1,6 @@
 package glslpractice.launcher;
 
-import IOException;
+import java.io.IOException;
 
 /**
  * 指定されたパスからmainを持ったクラスの一覧を取得する
@@ -8,7 +8,7 @@ import IOException;
 public class Launcher {
     public static void main(String[] args) {
         try {
-            ClassIndexer.getIndex(args[1]);
+            ClassIndexer.getIndex("glslpractice");
         }
         catch (IOException e) {
             e.printStackTrace();
